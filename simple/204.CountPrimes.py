406fbe097fa97543f580649bd3e0429e857d0e20
jiwaaying <jiwaayingxp@163.com>
2019-07-24 23:49:24 +0800

    add faster method

--- simple/204.CountPrimes.py
+++ simple/204.CountPrimes.py
@@ -5,8 +5,11 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(n+1):
+        if n < 3:
+            return 0
+        for i in range(n):
             if self.isPrime(i):
+                print i
                 count += 1
         return count
 
@@ -15,12 +18,37 @@ class Solution(object):
             return False
         if n == 2:
             return True
-        for i in range(2, n):
+        num = int(n**0.5) + 1
+        for i in range(2, num):
             if n % i == 0:
                 return False
         return True
 
+    def fasterCountPrimes(self, n):
+        isPrime = [0] * n
+        loopNum = int(n**0.5) + 1
+        for i in range(2, n):
+            isPrime[i] = True
+        for i in range(2, loopNum):
+            if not isPrime[i]:
+                continue
+            for j in range(i*i, n, i):
+                isPrime[j] = False
+                # j += i
+        count = 0
+        for i in range(2, n):
+            if isPrime[i]:
+                count += 1
+        return count
+
+
 if __name__ == '__main__':
     inputs = 10
+    import datetime
+    st = datetime.datetime.now()
     s = Solution()
-    print s.countPrimes(inputs)
+    # print s.countPrimes(inputs)
+    print s.fasterCountPrimes(inputs)
+    ed = datetime.datetime.now()
+    print ed
+    print st
