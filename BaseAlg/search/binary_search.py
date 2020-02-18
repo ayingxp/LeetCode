e45cef24befc363fb94f1972bd1c5ffd66dc1775
jiwaaying <jiwaayingxp@163.com>
2020-02-18 04:20:09 -0500

    add binary search

---
+++ BaseAlg/search/binary_search.py
@@ -0,0 +1,44 @@
+import time
+
+
+
+def binary_search(lst, target):
+    if not lst:
+        print('lst is empty!')
+        return None
+
+    if target < lst[0] or target > lst[-1]:
+        print('target is not in range!')
+        return None
+
+    left = 0
+    right = len(lst) - 1
+    mid = (left + right) // 2
+
+    while True:
+        if left > right:
+            return None
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] > target:
+            right = mid
+            mid = (left + right) // 2
+        elif lst[mid] < target:
+            left = mid
+            mid = (left + right) // 2
+
+
+    
+
+
+if __name__ == '__main__':
+   lst = list(range(10000)) 
+   
+   
+   target = 8888
+   s = time.time()
+   ind = binary_search(lst, target)
+   e = time.time()
+
+   print(ind)
+   print(e-s)
