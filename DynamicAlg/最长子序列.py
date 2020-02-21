21e39d954fe96481f3895f969a0ac7c5cf122ad4
jiwaaying <jiwaayingxp@163.com>
2020-02-21 06:00:28 -0500

    add content

--- DynamicAlg/最长子序列.py
+++ DynamicAlg/最长子序列.py
@@ -6,8 +6,7 @@ def get_longest_common_subsequence(a, b):
     dp[i][j]定义a[0]-a[i] 和 b[0]-b[j]的最长子序列长度
     """
     dp = [[0] * (len(b)+1) for _ in range(len(a) + 1)]
-    for k in dp:
-        print(k)
+    #path = [[0] * (len(b)+1) for _ in range(len(a) + 1)]
 
     for i in range(1, len(a) + 1):
         for j in range(1, len(b) + 1):
@@ -15,15 +14,16 @@ def get_longest_common_subsequence(a, b):
                 dp[i][j] = dp[i-1][j-1] + 1
             else:
                 dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-        for k in dp:
-            print(k)
-        print('**'*30)
+        # print('**'*30)
 
-    return dp[len(a)][len(b)]
+    return dp
 
 if __name__ == '__main__':
-    print('Hello World!')
     a = 'abcbdab'
     b = 'bdcaba'
+    print(a)
+    print(b)
     dp = get_longest_common_subsequence(a, b)
-    print(dp)
+    for k in dp:
+        print(k)
+    print(dp[-1][-1])
