b2f0ced3c5b524c08a1c5383baee07548f9428a7
jiwaaying <jiwaayingxp@163.com>
2020-03-21 06:42:46 -0400

    add content

--- recursion/bin_search.py
+++ recursion/bin_search.py
@@ -19,13 +19,34 @@ def bin_search_no_rec(lst, value):
 
     return None
 
+def bin_search_rec(lst, value, low, high):
+    if low > high:
+        return None
+    mid = (low + high) // 2
+
+    if lst[mid] == value:
+        return mid
+
+    else:
+        if lst[mid] < value:
+            low = mid + 1
+            res = bin_search_rec(lst, value, low, high)
+        else:
+            high = mid - 1
+            res = bin_search_rec(lst, value, low, high)
+
+    return res
+
+
+
 
 
 
 if __name__ == "__main__":
     data = [1, 7, 17, 18, 27, 29, 30, 35, 39, 41, 66, 67, 78, 82, 91, 92]
 
-    ind = bin_search_no_rec(data, 1)
+    # ind = bin_search_no_rec(data, 2)
+    ind = bin_search_rec(data, 18, 0, len(data)-1)
     print(ind)
     if ind:
         print(data[ind])
